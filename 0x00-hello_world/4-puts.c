d9a364fc70dc29e916288dc48f3829d799b7887e
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-02-16 12:47:53 -0800

    writing C programs task 4 & 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - using puts to print a string
+*Return: always ends in 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
+
