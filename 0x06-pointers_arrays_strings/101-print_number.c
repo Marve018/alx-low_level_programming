5151d32e5db508e5cd7dd9157f7865bc82b5b8e1
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-03-02 17:36:00 -0800

    pointers, arrays and strings task 0 to 12

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,10 +9,17 @@
  */
 void print_number(int n)
 {
+	unsigned int n1;
 
-	if (n % 2 == 0)
+	n1 = n;
+	if (n < 0)
 	{
-		n++;
-		_putchar(n + '0');
+		_putchar('-');
+		n1 = -n;
 	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
