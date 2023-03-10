58990a631db08efbc0eef0fa9e17626b93bf3368
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-03-09 16:19:10 -0800

    writing programs with recursion (new changes made)

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ *main_sqrt - function that finds the square root
+ *@a: number being calculated
+ *@b: iterator
+ *Return: the function
+ */
+int main_sqrt(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (main_sqrt(a, b + 1));
+}
+
+/**
  *_sqrt_recursion - function that returns the natural
  *square root of a number
  *@n: input number
@@ -8,5 +23,5 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n = )
+	return (main_sqrt(n, 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,28 @@
 #include "main.h"
 
 /**
+ *main_prime_number - helper function
+ *@n: number calculated
+ *@i: possible divisors
+ *Return: 1 if prime number, 0 if not
+ */
+int main_prime_number(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	return (main_prime_number(n, i - 1));
+}
+/**
  *is_prime_number - function that returns 1 if the input
  *integer is a prime number, otherwise return 0
- *@n: input
- *Return: prime number
+ *@n: number calculated
+ *Return: 1 if prime number, 0 if not
  */
 int is_prime_number(int n)
 {
-	if 
+	if (n <= 1)
+		return (0);
+	return (main_prime_number(n, n - 1));
 }
