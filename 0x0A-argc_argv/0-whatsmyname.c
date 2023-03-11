8f02058df66dbfb31377732b3ca137e364f44ceb
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-03-11 14:10:52 -0800

    codes solving all the tasks on the README.md file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - program that prints its name, followed by a new line.
+ *@argc: argument count
+ *@argv: array of arguments
+ *Return: 0 always (sucess)
+ */
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
