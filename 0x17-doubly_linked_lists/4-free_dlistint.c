4b2e4af6d4033bafe594d5048634e31509ed4251
Marve018 <okechukwuchukwuemeka018@gmail.com>
2023-08-20 12:34:22 -0700

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a list
+ * @head: variable pointer
+ * Return:
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	while (head != NULL)
+	{
+		dlistint_t *next = head->next;
+
+		free(head);
+		head = next;
+	}
+}
